4c4ea5090883355b8412a06cc19d84031cbb8f41
Asem Edress <asem.edress@gmail.com>
2022-11-06 18:13:06 +0100

    added the job service

--- src/services/job.service.jsx
+++ src/services/job.service.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_URL_SERVER_URL}`
+    baseURL: `${process.env.REACT_APP_URL_SERVER_URL || "http://localhost:5005"}`
+});
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem()
 })
